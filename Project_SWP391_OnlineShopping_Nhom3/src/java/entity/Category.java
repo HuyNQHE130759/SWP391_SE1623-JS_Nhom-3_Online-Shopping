e589f07e75bac2a10c01e020154499be23336e05
MightyLitten <hieunthe141740@fpt.edu.vn>
2022-11-07 00:21:49 +0700

    merge main

---
+++ Project_SWP391_OnlineShopping_Nhom3/src/java/entity/Category.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+
+public class Category {
+    private int cateId;
+    private String cateName;
+    private String image;
+    private boolean status;
+
+    public Category() {
+    }
+
+    public Category(int cateId, String cateName, String image, boolean status) {
+        this.cateId = cateId;
+        this.cateName = cateName;
+        this.image = image;
+        this.status = status;
+    }
+
+    public int getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(int cateId) {
+        this.cateId = cateId;
+    }
+
+    public String getCateName() {
+        return cateName;
+    }
+
+    public void setCateName(String cateName) {
+        this.cateName = cateName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+}
